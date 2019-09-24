ade9f1d2d97a5b28219e2e555c585f9fdbbbf1b0
Hammad Tahir <hammad.tahir@boldcommerce.com>
2019-09-24 14:40:22 -0500

    commit latest changes

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/hammadtahir/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hammadtahir/my-work-place/flutter_ride_share"
+export "FLUTTER_TARGET=/Users/hammadtahir/my-work-place/flutter_ride_share/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/hammadtahir/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
